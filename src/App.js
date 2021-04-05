e26091e025046d5783e0f772428a93a578fee712
Marisa Li <marisa.susan.li@gmail.com>
2021-04-05 12:02:13 -0400

    feat: add favorites list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import './App.css';
 import allCourses from './CourseDataRated.json';
 import Course from './Course';
+import { useState } from 'react';
 
 const App = () => {
+  const [favorites, setFavorites] = useState([]);
+
+  const addToFavorites = (course) => {
+    setFavorites([...favorites, course]);
+  };
+
+  const removeFromFavorites = (course) => {
+    const updated = favorites.filter((fav) => fav.id !== course.id);
+    setFavorites(updated);
+  };
+
   return (
     <div className='main'>
       <header className='main-header'>Welcome to MasterClass</header>
       <div className='course-list'>
         {allCourses.length
           ? allCourses.map((course) => (
-              <Course key={course.id} courseData={course} />
+              <Course
+                key={course.id}
+                courseData={course}
+                favorites={favorites}
+                addToFavs={addToFavorites}
+                removeFromFavs={removeFromFavorites}
+              />
             ))
           : 'There are no courses to view at this time.'}
       </div>
--- src/Course.js
+++ src/Course.js
@@ -1,6 +1,9 @@
 import './Course.css';
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Course = ({ courseData }) => {
+const Course = ({ courseData, favorites, addToFavs, removeFromFavs }) => {
   return (
     <div className='course-list-item'>
       <img
@@ -8,9 +11,24 @@ const Course = ({ courseData }) => {
         className={'item-img'}
         alt={courseData.instructor_name}
       />
+
       <div className='item-text'>
         <h3 className='item-name'>{courseData.instructor_name}</h3>
         <h3 className='item-title'>{courseData.title}</h3>
+      </div>
+
+      <div className={'heart-icon'}>
+        {favorites.includes(courseData) ? (
+          <FontAwesomeIcon
+            icon={faHeartSolid}
+            onClick={() => removeFromFavs(courseData)}
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faHeartRegular}
+            onClick={() => addToFavs(courseData)}
+          />
+        )}
       </div>
     </div>
   );
